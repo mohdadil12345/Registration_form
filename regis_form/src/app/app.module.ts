1d2b49d42e390b38fd8f656d32d58755e3b590c9
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2024-03-09 16:29:53 +0530

    css design for signup form

--- regis_form/src/app/app.module.ts
+++ regis_form/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupPageComponent } from './signup-page/signup-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { LoginPageComponent } from './login-page/login-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- regis_form/src/app/signup-page/signup-page.component.ts
+++ regis_form/src/app/signup-page/signup-page.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class SignupPageComponent {
 
+
+
+  handle_signup() {
+    console.log("form submit")
+  }
+
 }
